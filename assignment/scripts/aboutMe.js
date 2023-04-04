ab99646798c061158056bfcf0b1007e8529dfbf6
Halima Omar <omarhalima100@gmail.com>
2023-04-03 19:32:12 -0500

    completed question 17

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -68,12 +68,20 @@ if (pets < allowedPets) {
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
 
+
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
+if (luckyNumber === 1) {
+    console.log('First is the worst');
+} else if (luckyNumber ===2) {
+    console.log('Second is  the best');
+} else { (luckyNumber === 3) 
+    console.log('Third is the one with the polka dot dress');
+}
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
